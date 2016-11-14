e6cc2255a3bb93aaa628773016fbe5cf40cc21f7
Colin Seymour <colin@github.com>
2016-11-14 14:28:08 -0800

    Submitted roman_numerals

---
+++ roman-numerals/roman_numerals.rb
@@ -0,0 +1,33 @@
+class Fixnum
+  def to_roman
+    number_hash = {
+      1000 => "M",
+      900 => "CM",
+      500 => "D",
+      400 => "CD",
+      100 => "C",
+      90 => "XC",
+      50 => "L",
+      40 => "XL",
+      10 => "X",
+      9 => "IX",
+      5 => "V",
+      4 => "IV",
+      1 => "I"
+    }
+
+    output = ""
+    remainder = self
+    while remainder > 0
+      number_hash.each do |n, s|
+        output << number_hash[n] * (remainder/n).numerator if (remainder/n).numerator > 0
+        remainder = remainder.modulo(n)
+      end
+    end
+    output
+  end
+end
+
+module BookKeeping
+  VERSION = 2
+end
