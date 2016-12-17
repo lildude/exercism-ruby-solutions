cde078b9cd884595a27751cdc70ffbfde245ada1
Colin Seymour <colin@github.com>
2016-12-17 13:41:10 +0000

    Use simpler regex and move manipulation out of the constructor

--- word-count/word_count.rb
+++ word-count/word_count.rb
@@ -1,10 +1,10 @@
 class Phrase
   def initialize words
-    @words = words.downcase.scan(/\w+(?:'\w+)*/)
+    @words = words
   end
 
   def word_count
-    @words.each_with_object(Hash.new(0)) { |word,counts| counts[word] += 1 }
+    @words.downcase.scan(/\b[\w']+\b/).each_with_object(Hash.new(0)) { |word,counts| counts[word] += 1 }
   end
 end
 
