448666102c854080b7dc6ac72efd64907e46dbb4
Colin Seymour <lildood@gmail.com>
2017-07-31 18:46:40 +0100

    Refactor

--- series/series.rb
+++ series/series.rb
@@ -1,13 +1,10 @@
 class Series
   def initialize str
-    @str = str
+    @str = str.chars
   end
 
   def slices(num)
     raise ArgumentError if num > @str.length
-    @str.split(//).each_with_index.each_with_object([]) do |(_, i), k|
-      r = @str[i..i+num-1]
-      k << r if r.length == num
-    end
+    @str.each_cons(num).map(&:join)
   end
 end
