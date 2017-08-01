4cd3a6a47e44d7f628c2a1116dbef8b70824e1b3
Colin Seymour <lildood@gmail.com>
2017-08-01 17:56:37 +0100

    Spaces not tabs

--- phone-number/phone_number.rb
+++ phone-number/phone_number.rb
@@ -1,8 +1,8 @@
 class PhoneNumber
-	def self.clean phone_number
-		clean_number = phone_number.gsub(/\D/, "").gsub(/^1/,"")
-		return clean_number if(("2".."9") === clean_number[3] && clean_number.length == 10 )
-	end
+  def self.clean phone_number
+    clean_number = phone_number.gsub(/\D/, "").gsub(/^1/,"")
+    return clean_number if(("2".."9") === clean_number[3] && clean_number.length == 10 )
+  end
 end
 
 module BookKeeping
